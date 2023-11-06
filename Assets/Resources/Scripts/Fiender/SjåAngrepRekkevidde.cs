1d04a5862651a1d2e4872542427cd18e1a89a75e
JohannesAldal-Jobloop <johannes@jobloop.no>
2023-11-06 15:15:32 +0100

    Begun on enemy range script

---
+++ Assets/Resources/Scripts/Fiender/SjåAngrepRekkevidde.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SjåAngrepRekkevidde : MonoBehaviour
+{
+    public float sjåRekkevidde = 20;
+    public float angrepRekkevidde = 10;
+
+    public bool serLayerTarget;
+    public bool angripLayerTarget;
+
+    public LayerMask layerTarget;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        serLayerTarget = Physics.CheckSphere(transform.position, sjåRekkevidde, layerTarget);
+        angripLayerTarget = Physics.CheckSphere(transform.position, angrepRekkevidde, layerTarget);
+    }
+}
