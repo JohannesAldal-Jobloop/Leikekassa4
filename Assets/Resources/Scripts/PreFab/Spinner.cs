0ca6a520706b3a234a3b25c930e4106e6a888880
JohannesAldal-Jobloop <johannes@jobloop.no>
2024-02-14 11:23:41 +0100

    Made a script that rotates the GameObject its on

---
+++ Assets/Resources/Scripts/PreFab/Spinner.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spinner : MonoBehaviour
+{
+    [SerializeField] private float rotationSpeed = 1.0f;
+
+    [SerializeField] private Vector3 spinnVector3;
+
+    //public GameObject gameObjectToSpinn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.Rotate(spinnVector3, rotationSpeed);
+    }
+
+    private void SpinnGameObject(GameObject spinnGO, float spinnSpeed, /*string spinnDirection,*/ Vector3 spinnVector3)
+    {
+        spinnGO.transform.Rotate(spinnVector3, spinnSpeed);
+
+        //if (spinnDirection == "x")
+        //{
+            
+        //}
+        //else if(spinnDirection == "y")
+        //{
+
+        //}
+        //else if (spinnDirection == "z")
+        //{
+
+        //}
+    }
+}
