2b1135178427bf1b33582b00a3caec2d98d42e18
JohannesAldal-Jobloop <johannes@jobloop.no>
2024-02-29 15:03:42 +0100

    Made an item class be saved in a list
    I Made the itemClass class to be able to be put on a GameObject and then when its hit by the player it will be saved in a list in the inventory script.

--- Assets/Resources/Scripts/Inventar/InventoryScript.cs
+++ Assets/Resources/Scripts/Inventar/InventoryScript.cs
@@ -6,10 +6,24 @@ public class InventoryScript : MonoBehaviour
 {
     public bool inventortOpen = false;
 
+    private GameObject weaponInventory;
+    private GameObject armorInventory;
+    private GameObject itemsInventory;
+
+    public List<ItemClass> weaponsInInvetoryList = new List<ItemClass>();
+    public List<ItemClass> armorInInvetoryList = new List<ItemClass>();
+    public List<ItemClass> itemsInInvetoryList = new List<ItemClass>();
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        weaponInventory = GameObject.Find("Weapons");
+        armorInventory = GameObject.Find("Armor");
+        itemsInventory = GameObject.Find("Items");
+
+        weaponInventory.SetActive(false);
+        armorInventory.SetActive(false);
+        itemsInventory.SetActive(false);
     }
 
     // Update is called once per frame
@@ -25,5 +39,24 @@ public class InventoryScript : MonoBehaviour
         }
     }
 
+    public void ShowWeaponsInInventory()
+    {
+        weaponInventory.SetActive(true);
+        armorInventory.SetActive(false);
+        itemsInventory.SetActive(false);
+    }
 
+    public void ShowArmorInInventory()
+    {
+        weaponInventory.SetActive(false);
+        armorInventory.SetActive(true);
+        itemsInventory.SetActive(false);
+    }
+
+    public void ShowItemsInInventory()
+    {
+        weaponInventory.SetActive(false);
+        armorInventory.SetActive(false);
+        itemsInventory.SetActive(true);
+    }
 }
--- Assets/Resources/Scripts/Inventar/ItemClass.cs
+++ Assets/Resources/Scripts/Inventar/ItemClass.cs
@@ -4,20 +4,40 @@ using UnityEngine;
 
 public class ItemClass : MonoBehaviour
 {
+    public string[] itemTags = { };
     public string itemName;
     public string itemDescription;
     public int itemSellValue;
 
+    private InventoryScript inventoryScript;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        inventoryScript = GameObject.Find("SpelSjef").GetComponent<InventoryScript>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "SpelerFPS")
+        {
+            inventoryScript.weaponsInInvetoryList.Add(this);
+            gameObject.SetActive(false);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "SpelerFPS")
+        {
+            inventoryScript.weaponsInInvetoryList.Add(this);
+            gameObject.SetActive(false);
+        }
     }
 }
