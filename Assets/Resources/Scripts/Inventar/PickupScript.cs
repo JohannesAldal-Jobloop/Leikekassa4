5f8fb66f6d7f0c702fc592c8d951502af97d5dd9
JohannesAldal-Jobloop <johannes@jobloop.no>
2024-03-21 15:53:42 +0100

    Updated to do list

--- Assets/Resources/Scripts/Inventar/PickupScript.cs
+++ Assets/Resources/Scripts/Inventar/PickupScript.cs
@@ -1,17 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PickupScript : MonoBehaviour
 {
+    [SerializeField] private int opacity;
+    [SerializeField] private float interactWaitForSeconds;
+
     public bool interactPickup = false;
 
-    InventoryScript inventoryScript;
+    private KeyCode interactKey = KeyCode.E;
+
+    private GameObject interactPromptGO;
+    private TextMeshProUGUI interactPromptText;
+    private Image interactProgressImg;
+
+    private InventoryScript inventoryScript;
 
     // Start is called before the first frame update
     void Start()
     {
+        interactPromptGO = GameObject.Find("SingleKey");
+        interactPromptText = interactPromptGO.GetComponentInChildren<TextMeshProUGUI>();
+        interactProgressImg = interactPromptGO.GetComponentInChildren<Image>();
+        interactPromptText.text = interactKey.ToString();
         inventoryScript = GameObject.Find("SpelSjef").GetComponent<InventoryScript>();
     }
 
@@ -71,7 +86,46 @@ public class PickupScript : MonoBehaviour
             {
                 //--------- Interact pickup ----------
 
+                interactPromptGO.SetActive(true);
+                if (Input.GetKey(interactKey) && opacity <= 255)
+                {
+                    Color newOpacity = interactProgressImg.GetComponent<Color>();
 
+                    opacity++;
+                    newOpacity.a = opacity;
+
+                    if(opacity <= 255)
+                        interactProgressImg.color = newOpacity;
+                }
+
+                if(opacity >= 255)
+                {
+                    // Checks what type of item itemToPickUp and
+                    // adds the item to the correct inventory list.
+                    if (itemToPickUp.itemTags[0] == inventoryScript.inventoryCategoryTags[0])
+                    {
+                        // weapons
+                        inventoryScript.weaponsInInvetoryList.Add(itemToPickUp);
+
+                        other.gameObject.SetActive(false);
+
+                    }
+                    else if (itemToPickUp.itemTags[0] == inventoryScript.inventoryCategoryTags[1])
+                    {
+
+                        // armor
+                        inventoryScript.armorInInvetoryList.Add(itemToPickUp);
+
+                        other.gameObject.SetActive(false);
+                    }
+                    else if (itemToPickUp.itemTags[0] == inventoryScript.inventoryCategoryTags[2])
+                    {
+                        // items
+                        inventoryScript.itemsInInvetoryList.Add(itemToPickUp);
+
+                        other.gameObject.SetActive(false);
+                    }
+                }
 
                 //------------------------------------
             }
@@ -109,6 +163,20 @@ public class PickupScript : MonoBehaviour
 
 
         }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.transform.tag == "PickupItem")
+        {
+            interactPromptGO.SetActive(false);
+        }
+    }
+
+    IEnumerator InteractProgress()
+    {
+        opacity++;
+        yield return new WaitForSeconds(interactWaitForSeconds);
     }
 
 }
